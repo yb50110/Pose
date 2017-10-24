0e13e7c3d4e851e51fb41363223f6a910ad949d9
yunhaseo <yunha@b302.nl>
2017-10-24 01:05:05 -0500

    work on header menu and smooth scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SpeakersComponent } from './home/speakers/speakers.component';
-import { ScheduleComponent } from './home/schedule/schedule.component';
-import { LodgingComponent } from './home/lodging/lodging.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {SpeakersComponent} from './home/speakers/speakers.component';
+import {ScheduleComponent} from './home/schedule/schedule.component';
+import {LodgingComponent} from './home/lodging/lodging.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    SpeakersComponent,
-    ScheduleComponent,
-    LodgingComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        HeaderComponent,
+        FooterComponent,
+        SpeakersComponent,
+        ScheduleComponent,
+        LodgingComponent,
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+    selector: 'app-header',
+    templateUrl: './header.component.html'
 })
+
 export class HeaderComponent implements OnInit {
 
-  isOpen: false;
+    constructor() {}
 
-  constructor() { }
+    isOpen: boolean;
 
-  ngOnInit() {
-  //   this.isOpen: false;
-  }
-  //
-  // private toggleOpen() {
-  //   this.isOpen: true;
-  // }
+    ngOnInit() {
+        this.isOpen = false;
+    }
 
+    openMenu() {
+        this.isOpen = true;
+    }
+    closeMenu() {
+        this.isOpen = false;
+    }
+
+    scrollTo(section) {
+        const menu = document.getElementById('pose-menu');
+        const goto = document.getElementById('pose-section-' + section);
+        // smooth scroll to section
+        $('html,body').animate({
+            scrollTop: $(goto).offset().top
+        }, 1000);
+        // close menu
+        this.isOpen = false;
+    }
+
+    // fixed header
+    // spin of 18
+    // moving of pose line
 }
