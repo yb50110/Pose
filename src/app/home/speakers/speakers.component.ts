fc06652bc0299c2d9bdc555e8ddf75c83526de3b
yunhaseo <yunha@b302.nl>
2017-10-24 10:51:19 -0500

    some work on speakers

--- src/app/home/speakers/speakers.component.ts
+++ src/app/home/speakers/speakers.component.ts
@@ -1,14 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-speakers',
-  templateUrl: './speakers.component.html',
+    selector: 'app-speakers',
+    templateUrl: './speakers.component.html',
+
 })
 export class SpeakersComponent implements OnInit {
+    speakers = [
+        {
+            fName: 'Airi',
+            lName: 'Nakamura',
+            imageUrl: 'airi-nakamura-3.png',
+            viewing: true
+        },
+        {
+            fName: 'Allycia',
+            lName: 'London',
+            imageUrl: 'allycia-london-3.png',
+            viewing: false
+        },
+        {
+            fName: 'Chico',
+            lName: 'Shakil',
+            imageUrl: 'chico-shakil-3.png',
+            viewing: false
+        },
+        {
+            fName: 'Colene',
+            lName: 'Berny',
+            imageUrl: 'colene-berny-3.png',
+            viewing: false
+        },
+        {
+            fName: 'Daniel',
+            lName: 'Jeptha',
+            imageUrl: 'daniel-jeptha-3.png',
+            viewing: false
+        },
+        {
+            fName: 'Warren',
+            lName: 'Kumar',
+            imageUrl: 'warren-kumar-3.png',
+            viewing: false
+        }
+    ];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    // when scroll, move image and then set that to be visible=true and the prev as visible=false
 }
